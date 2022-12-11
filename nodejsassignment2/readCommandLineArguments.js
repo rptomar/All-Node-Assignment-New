e24f254facaec0b30af0e7fdd04c0741c6d7b2dc
rptomar <111333993+rptomar@users.noreply.github.com>
2022-12-11 02:42:10 -0800

    this is assignment 2

---
+++ nodejsassignment2/readCommandLineArguments.js
@@ -0,0 +1,3 @@
+var name = process.argv[2];
+console.log("hello ", name);
+
---
+++ nodejsassignment2/readEnvVariable.js
@@ -0,0 +1,4 @@
+//process.env.USERNAME = "hello Ravi";
+//console.log("Hello", process.env.USERNAME);
+let name = process.env.USERNAME;
+console.log(`Hello  ${name}`);
